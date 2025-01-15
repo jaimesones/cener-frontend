fe681df67ed9e4b9e05f9ef9ca88fb59d50e98f6
Jaime <jaimechacons@hotmail.com>
2025-01-15 01:47:51 +0100

    Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', loadChildren: () => import('./pages/home.module').then(module => module.HomeModule)}, // lazy load
-];
 
+const routes: Routes = [
+  { 
+    path: 'home', // Mantenemos 'home' en la raíz
+    loadChildren: () => import('./pages/home.module').then(module => module.HomeModule) 
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección a /home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
